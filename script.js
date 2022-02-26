357b9fa38920b7e73ff193329cf13d9e335cedec
mehadi <mehadi.contact@gmail.com>
2022-02-26 18:29:11 +0600

    customise ui deign and checkbox

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function Generate() {
-    ////
+    var length = document.getElementById("password-length").value;
+    console.log(length);
+    var specialCharacters = document.getElementById("special-characters").checked;
+    console.log(specialCharacters);
+    var digit = document.getElementById("digit").checked;
+    console.log(digit);
+    var lower_characters = document.getElementById("lower-characters").checked;
+    console.log(lower_characters);
+    var upper_characters = document.getElementById("upper-characters").checked;
+    console.log(upper_characters);
 }
